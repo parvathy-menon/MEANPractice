8842e8a00e93efe54d6106e91118af6aeb63acf8
Parvathy Menon <parvathy.menon1997@gmail.com>
2017-02-21 11:35:18 -0800

    practice

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
+//Connect to Database
 mongoose.connect(config.database);
 
+//On Connection
 mongoose.connection.on('connected', () => {
   console.log('Connected to database '+config.database);
+});
+
+//On Error
+mongoose.connection.on('error', (err) => {
+  console.log('Database error: '+err);
 });
 
 const app = express();
@@ -27,6 +34,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //Body Parser Middleware
 app.use(bodyParser.json());
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
 
 app.use('/users', users);
 
